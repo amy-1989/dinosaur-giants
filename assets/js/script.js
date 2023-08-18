69674a09472f659458d91d939b10d6467f2d11c6
amy-1989 <>
2023-08-18 12:30:38 +0000

    navbar styles finalised

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-/* for toggling between displaying and hiding the nav links when the burger menu is clicked */
-function myFunction() { 
-    var x = document.getElementById("navLinks");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
-    }
-}
